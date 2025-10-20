f1c818e985d31b7f7f151fd309724275a53ecbd5
Koray <korayduzgun@outlook.com>
2025-10-20 09:52:36 +0200

    add plotting and analysis utils

---
+++ src/utils/growth_analysis.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def avg_growth(episodes):
+    max_len = max(len(e) for e in episodes)
+    arr = []
+    for e in episodes:
+        padded = np.array(
+            [np.pad(e, ((0, max_len-len(e)), (0, 0)), mode='edge')])[0]
+        arr.append(padded)
+    data = np.mean(arr, axis=0)
+    avg_len = np.mean(data)
+    alive = np.mean([(np.count_nonzero(h > 0.5)/len(h)) for h in data])
+    return data, avg_len, alive
